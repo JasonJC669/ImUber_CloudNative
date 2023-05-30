56549bfd8fe558430b6245090a856edde78f775f
unknown <j669404@gmail.com>
2023-05-30 17:19:31 +0800

    Modify Web icon, Web font

--- client/src/pages/Links.jsx
+++ client/src/pages/Links.jsx
@@ -25,11 +25,15 @@ const Wrapper = styled.div`
 `
 
 const Title = styled.h1`
-    font-size: 24px;
+    font-family: 'Boogaloo';
+    font-size: 58px;
+    text-align: center;
     margin-bottom: 20px;
 `
 
 const Label = styled.label`
+    font-family: 'Kalam';
+    font-size: 28px;
     margin: 5px;
 `
 
@@ -45,15 +49,12 @@ const Button = styled.button.attrs({
     margin: 15px 15px 15px 5px;
 `
 
-
-
 class Links extends Component {
-
     constructor(props) {
         super(props)
 
         this.state = {
-            ID: '',
+            ID: 'Max',
             chose_User_type: false,
             driver_flag: false,
             passenger_flag: false,
@@ -66,69 +67,55 @@ class Links extends Component {
     }
 
     Login = async () => {
-        const {ID} = this.state
-
+        const { ID } = this.state
         this.setState({ ID: ID, chose_User_type: true })
     }
 
-
     Driver_Login = async () => {
-        const {ID} = this.state
+        const { ID } = this.state
         const Driver_ID = { ID }
-    
         await api.Driver_login(Driver_ID)
-        //window.alert(`Driver login successfully`)
+        // window.alert(`Driver login successfully`)
         this.setState({ ID: '', driver_flag: true })
     }
 
     Passenger_Login = async () => {
-        const {ID} = this.state
+        const { ID } = this.state
         const Driver_ID = { ID }
-    
         await api.Passenger_login(Driver_ID)
-        //window.alert(`Passenger login successfully`)
+        // window.alert(`Passenger login successfully`)
         this.setState({ ID: '', passenger_flag: true })
     }
 
-
-
     render() {
-        const {ID, chose_User_type} = this.state
+        const { ID, chose_User_type } = this.state
 
         if (chose_User_type) {
             return (
                 <Container>
                     <Wrapper>
-                        <Title>Welcome to Uber</Title>
-                        <Label>ID: </Label>
-                            <InputText
-                                type="text"
-                                value={ID}
-                                onChange={this.handle_Input_ID}
-                            />
-
-                            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                                <Button onClick={this.Driver_Login}>司機</Button>
-                                <Button onClick={this.Passenger_Login}>乘客</Button>
-                            </div>
+                        <Title>Welcome to ImUber</Title>
+                        <Label>Hi {ID}! Are you going to be a passenger or a driver today?</Label>
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <Button onClick={this.Driver_Login}>司機</Button>
+                            <Button onClick={this.Passenger_Login}>乘客</Button>
+                        </div>
                     </Wrapper>
                 </Container>
             )
         }
 
-
         return (
             <Container>
                 <Wrapper>
-                    <Title>Welcome to Uber</Title>
-                    <Label>ID: </Label>
-                        <InputText
-                            type="text"
-                            value={ID}
-                            onChange={this.handle_Input_ID}
-                        />
-
-                        <Button onClick={this.Login}>登入</Button>
+                    <Title>Welcome to ImUber</Title>
+                    <Label>Please enter your name: </Label>
+                    <InputText
+                        type="text"
+                        value={ID}
+                        onChange={this.handle_Input_ID}
+                    />
+                    <Button onClick={this.Login}>登入</Button>
                 </Wrapper>
             </Container>
         )

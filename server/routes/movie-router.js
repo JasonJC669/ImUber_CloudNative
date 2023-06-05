b6adac0118d739739a5f75f7dd0d2f00cc4e0d94
yufanh <yufanhuang88@gmail.com>
2023-06-05 23:37:56 +0800

    Update group-ctrl.js: joinGroup

--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -6,15 +6,16 @@ const GroupCtrl = require('../controllers/group-ctrl')
 const router = express.Router()
 
 router.post('/login/passenger', PassengerCtrl.addPassenger)
-router.post('/login/driver', DriverCtrl.addDriver)
+// router.post('/login/driver', DriverCtrl.addDriver)
 // router.post('/login/driver', GroupCtrl.createGroup)
 // router.post('/login/driver', GroupCtrl.joinGroup)
-// router.post('/login/driver', GroupCtrl.getNearGroups)
+router.post('/login/driver', GroupCtrl.getNearGroups)
 
 
 router.post('/group/getNear', GroupCtrl.getNearGroups)
 router.post('/group/creat', GroupCtrl.createGroup)
 router.post('/group/get', GroupCtrl.getGroup)
+router.post('/group/join', GroupCtrl.joinGroup)
 // router.put('/movie/:id', MovieCtrl.updateMovie)
 // router.delete('/movie/:id', MovieCtrl.deleteMovie)
 // router.get('/movie/:id', MovieCtrl.getMovieById)

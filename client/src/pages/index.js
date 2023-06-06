41dae85789214ab7853c8b08a5a56f55869f6c6b
unknown <j669404@gmail.com>
2023-06-06 23:52:27 +0800

    delete no use files

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,10 +1,7 @@
-import DriverLogin from './DriverLogin'
-import PassengerLogin from './PassengerLogin'
-import Driver_road from './Driver_road'
 import Links from './Links'
 import PassengerMap from './PassengerMap'
 import DriverMap from './DriverMap'
 import DriverGroup from './DriverGroup'
 import PassengerGroup from './PassengerGroup'
 
-export { DriverLogin, PassengerLogin, Driver_road, Links, PassengerMap, DriverMap, DriverGroup, PassengerGroup }
+export { Links, PassengerMap, DriverMap, DriverGroup, PassengerGroup }
